25d4e89d82fbb75365467520bfdcbef1f8818c92
Philip Riecks <philip.riecks@herzomedia.net>
2016-09-06 13:21:26 +0200

    Abänderung

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ fs.readFile('foo.js', 'utf8', function (err, data) {
     if (err) {
         throw err;
     }
-  //  console.log(beautify(data, { indent_size: 2 }));
+    console.log(beautify(data, { indent_size: 2 }));
 	var content = beautify(data, {indent_size:2});
 	fs.writeFileSync('result.js',content, 'utf8');
 });
