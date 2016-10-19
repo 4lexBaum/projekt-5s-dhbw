087c53cc4b5544f470d6e11abd745f011e984e34
BoboDance <fabi.otto@t-online.de>
2016-10-19 21:00:02 +0200

    Docker-compose is runnable. ActiveMQ still buggy.

--- Dockerfiles/ActiveMQ/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# start activemq
-/activemq/bin/activemq start &
-
-# run endless loop to keep alive
-/usr/bin/tail -f /dev/null
--- Dockerfiles/Taktstrasse/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# start simulation
-java -jar TaktstrasseOpcServer-0.0.1-SNAPSHOT.jar -amqp tcp://localhost:32772 -d 1000 -kafka localhost:9092 -o /tmp -topic prodData & 
-# wait for process
-sleep 2 
-
-# run endless loop to keep alive
-/usr/bin/tail -f /dev/null
