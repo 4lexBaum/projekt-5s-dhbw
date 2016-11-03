ef07a92e1d5adecd562c2fa6868ba7e7d0adc950
PharmaPhil <riecks95@gmail.com>
2016-11-03 17:45:30 +0100

    node app is working in docker compose

--- Dockerfiles/node/nodeapp/server.js
+++ Dockerfiles/node/nodeapp/server.js
@@ -7,35 +7,39 @@ var kafka = require('kafka-node');
 var Consumer = kafka.Consumer;
 var Client = kafka.Client;
 var Offset = kafka.Offset;
+setTimeout(function () {
 
-var client = new Client("kafka");
-var topics = [{
-    topic: "prodData"
-}];
-var options = {
-    autoCommit: false,
-    fetchMaxWaitMs: 1000,
-    fetchMaxBytes: 1024 * 1024,
-    offset: 0
-};
-var consumer = new Consumer(client, topics, options);
-var offset = new Offset(client);
+    var client = new Client("kafka");
+    var topics = [{
+        topic: "prodData"
+    }];
+    var options = {
+        autoCommit: false,
+        fetchMaxWaitMs: 1000,
+        fetchMaxBytes: 1024 * 1024,
+        offset: 0
+    };
+    var consumer = new Consumer(client, topics, options);
+    var offset = new Offset(client);
 
-app.get('/', function (req, res) {
-    res.sendfile('./index.html');
-});
-
-io.on('connection', function (socket) {
-    console.log('a user connected');
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
-    });
-    consumer.on('message', function (message) {
-        socket.emit("test", message.value);
+    app.get('/', function (req, res) {
+        res.sendfile('./index.html');
     });
 
-});
+    io.on('connection', function (socket) {
+        console.log('a user connected');
+        socket.on('disconnect', function () {
+            console.log('user disconnected');
+        });
+        consumer.on('message', function (message) {
+            socket.emit("test", message.value);
+        });
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
-});
+    });
+
+    http.listen(3000, function () {
+        console.log('listening on *:3000');
+    });
+
+
+}, 5000);
