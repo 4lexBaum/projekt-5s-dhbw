12fa1c3deea8b9f138e0e229d48dec3f7df338fe
PharmaPhil <philip@riecks.io>
2016-11-05 13:38:17 +0100

    Update entrypoint.sh
    changed location of spectral analysis files

--- Dockerfiles/taktstrasse/entrypoint.sh
+++ Dockerfiles/taktstrasse/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # start simulation
-java -jar TaktstrasseOpcServer-0.0.1-SNAPSHOT.jar -amqp tcp://activemq:61616 -d 15000 -kafka kafka:9092 -o /temp -topic prodData &
+java -jar TaktstrasseOpcServer-0.0.1-SNAPSHOT.jar -amqp tcp://activemq:61616 -d 15000 -kafka kafka:9092 -o /spektralData -topic prodData &
 
 # start backend application (consumer)
 java -jar Backend-Application-0.0.1-SNAPSHOT.jar
