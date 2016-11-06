e69edbe81f0dc0b3baaadc5c090291c2ad0d5351
Philip Riecks <philip@riecks.io>
2016-11-06 16:09:48 +0100

    master clean up

--- kafkaConsumer/src/main/java/consumer/Consumer.java
+++
@@ -1,9 +0,0 @@
-package consumer;
-
-/**
- * Interface Consumer.
- * All consumers implement this interface.
- * @author Daniel
- *
- */
-public interface Consumer {}
--- kafkaConsumer/src/main/java/model/dataModels/Data.java
+++
@@ -1,9 +0,0 @@
-package model.dataModels;
-
-/**
- * Abstract Class Data.
- * All data types inherit from this class.
- * @author Daniel
- *
- */
-public abstract class Data {}
