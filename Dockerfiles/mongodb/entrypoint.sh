8b0b415617dd96ed1f5c66f8de78a78d4b8dca86
Philip Riecks <philip@riecks.io>
2016-11-09 18:48:59 +0100

    fixed collection name for mongoldb

--- Dockerfiles/mongodb/entrypoint.sh
+++ Dockerfiles/mongodb/entrypoint.sh
@@ -2,7 +2,7 @@
 /usr/bin/mongod &
 
 # Import the existing objects to mongoDB
-/usr/bin/mongoimport -d taktstrasse -c manufacutringData /data/historicData.json
+/usr/bin/mongoimport -d oip_taktstrasse -c manufacutringData /data/historicData.json
 
 # run endless loop to keep alive
 /usr/bin/tail -f /dev/null
